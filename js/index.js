2cf6ba6261028595cfd48807f02c3e5a06ad4c46
SunLu <sunlu@Sunlu-Mac-Pro.local>
2016-07-18 19:32:02 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var vm = new Vue ({
+  data: {
+    servants: {
+      "alttila": {
+        "level": 4,
+        "skill1": 4,
+        "skill2": 4,
+        "skill3": 6
+      }
+    }
+  },
+  method: {
+    
+  }
+});
